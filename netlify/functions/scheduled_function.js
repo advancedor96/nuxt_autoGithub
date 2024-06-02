345ad16eabb32f2c584cd017769a435cf47c43cb
Ding <advancedor96@gmail.com>
2024-06-02 16:33:11 +0200

    try chatgpt

--- netlify/functions/scheduled_function.js
+++ netlify/functions/scheduled_function.js
@@ -1,5 +1,12 @@
-export default async (req) => {
-  const { next_run } = await req.json()
+// export default async (req) => {
+//   const { next_run } = await req.json()
 
-  console.log("Received event! Next invocation at:", next_run)
-}
+//   console.log("Received event! Next invocation at:", next_run)
+// }
+
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, World" })
+  };
+};
