0080f74ee0e3cc590f2b103fc1929c40e1ebbf59
Ding <advancedor96@gmail.com>
2024-06-02 16:48:23 +0200

    try blog

--- netlify/functions/scheduled_function.js
+++ netlify/functions/scheduled_function.js
@@ -1,12 +1,21 @@
-// export default async (req) => {
-//   const { next_run } = await req.json()
+const fetch = require('node-fetch')
+import { schedule } from '@netlify/functions'
 
-//   console.log("Received event! Next invocation at:", next_run)
-// }
+// This is sample build hook
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/665c8595288c9ef2994b217c'
 
-exports.handler = async function(event, context) {
+const handler =  schedule('0 * * * *', async () => {
+  await fetch(BUILD_HOOK, {
+    method: 'POST'
+  }).then((response) => {
+    console.log('Build hook response:', response.json())
+  })
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello, World" })
-  };
-};
+    statusCode: 200
+  }
+})
+
+export {
+  handler
+}
