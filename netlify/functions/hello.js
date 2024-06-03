1b2b1753007b6d16ec5f95e618be748a5a8e06af
Ding <advancedor96@gmail.com>
2024-06-03 17:07:18 +0200

    udpate

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 export default async (req, context) => {
-  const data = await fetch('/api/hehe')
+  const data = await fetch('https://cheatongithub.netlify.app/api/hehe')
 
-  console.log('呼叫了api！！呼叫了！！呼叫了！！呼叫了！！來了:');
+  console.log('呼叫了api : https://cheatongithub.netlify.app/api/hehe');
 
 };
 export const config = {
