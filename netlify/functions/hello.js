4fd864988e1ca92a07cdc036687b21941ec711ba
Ding <advancedor96@gmail.com>
2024-06-03 17:00:39 +0200

    update

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,7 @@
 export default async (req, context) => {
   // const { next_run } = await fetch('http://localhost:8888/api/hehe')
 
-  console.log('呼叫了api！！呼叫了！！呼叫了！！呼叫了！！來了:',next_run);
+  console.log('呼叫了api！！呼叫了！！呼叫了！！呼叫了！！來了:');
 
 };
 export const config = {
