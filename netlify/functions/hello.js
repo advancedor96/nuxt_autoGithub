ed5453cb99e7374b99b2db809e47a8aa82ff6788
Ding <advancedor96@gmail.com>
2024-06-03 17:13:27 +0200

    只用hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,9 +1,25 @@
 export default async (req, context) => {
+  console.log(getCurrentFormattedDateTime() + '準備呼叫api....');
+  
   const data = await fetch('https://cheatongithub.netlify.app/api/hehe')
 
-  console.log('呼叫了api : https://cheatongithub.netlify.app/api/hehe');
+  console.log('完成。\n\n');
 
 };
+
+function getCurrentFormattedDateTime() {
+  const now = new Date();
+
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // getMonth() 從 0 開始，所以需要加 1
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+
+  const formattedDateTime = `${month}/${day} ${hours}:${minutes}`;
+  return formattedDateTime;
+}
+
+
 export const config = {
   schedule: "* * * * *"
 }
